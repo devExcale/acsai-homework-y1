0872e01ff295060ef0dc73b52f058f4e11dec7a5
Emanuele Scaccia <dev_excale@hotmail.com>
2021-11-06 22:25:35 +0100

    Completed HW2

--- HW2-req/program01.py
+++ HW2-req/program01.py
@@ -1,104 +1,27 @@
 # -*- coding: utf-8 -*-
 
-""" In the game "who screams louder," two players A and B, generate
-sequences of variable length values. Each value is represented by a
-single character. The sequences can be of different lengths because
-the values can be separated by one (or more) whitespaces
-and tabs ('\t'). The number of non-space characters is, however, equal
-for each sequence.
-
-Each element of the sequence of A is compared with the corresponding
-element of the B's sequence, and a point is assigned:
-- to the player who generated the highest value (for example A), if
-  the difference between the value of A and the value of B is less
-  than or equal to a parameter k decided at the beginning of the
-  challenge
-- to the player who has generated the lowest value (for example B), if
-  the difference between the value of A and the value of B is greater
-  than k (i.e., A has failed)
-- to none, in case of a tie.
-At the end of the assignment, whoever scored the most points wins. In
-the case of a tie, the player who generated the sequence with the
-lower total sum of values wins. In the case of a further tie, the
-player with the first sequence in lexicographic order wins. It cannot
-happen that two players generate precisely the same sequence of
-values.
-
-It is necessary to create a function that evaluates the ranking of a
-"who screams louder" tournament. The function takes as input a list of
-strings and a k parameter, and returns the final ranking of the
-tournament, as a list. The string in position "i" in the input list
-corresponds to the player "i"'s sequence of values.  In the
-tournament, each player challenges all the others with their own
-sequence: thus, if there are n players, each player will make n-1
-challenges. The number of winning challenges determines the position in
-the ranking. In case of a tie, the players are ordered according to
-their initial position.
-
-Example of "who screams louder" tournaments between three players.
-
-	If k=2 and the list is ["aac", "ccc", "caa"], then
-		the challenge 0, 1 is won by 1 by 2 points to 0, since the
-			difference between "c" and "a" is less than or equal to 2
-		the challenge 0, 2 is a 1 to 1 draw, the two sequences have
-			equal sum, but 0 wins because "aac" < "caa"
-		the challenge 1, 2 is won by 1 by 2 points to 0, since the
-			difference between "c" and "a" is less than or equal to 2.
-
-		In the end 0 has 1 challenge, 1 has 2 challenges and 2 has 0
-			challenges, so the final ranking will be [1, 0, 2].
-
-	If k=1 and the list is ["aac", "ccc", "caa"], then
-		the challenge 0, 1 is won by 0 by 2 points to 0, since the
-			difference between "c" and "a" is greater than 1
-		the challenge 0, 2 is a 1 to 1 tie, the two sequences have
-			equal sum equal, but 0 wins because "aac" < "caa".
-		the challenge 1, 2 is won by 2 for 2 points to 0, since the
-			difference between "c" and "a" is greater than 1.
-
-		In the end 0 has 2 challenges, 1 has 0 challenges and 2 has 1
-			challenge, so the final ranking will be [0, 2, 1].
-
-	If k=10 and the list is [ "abc", "dba" , "eZo"], then
-		the challenge 0, 1 is a tie, but 0 wins because its sequence
-			has lower sum
-		the challenge 0, 2 is won by 0 by 2 points to 1, because 2 is
-			wrong with the letter 'o' against 'c'
-		the challenge 1, 2 is won by 1 for 2 points to 1, because 2 is
-			fails with the letter 'o' vs. 'a'.
-
-		In the end 0 has 2 challenges, 1 has 1 challenge and 2 has 0
-			challenges, so the final ranking will be [0, 1, 2].
-
-	If k=50 and the list is [ "A ƐÈÜ", "BEAR" , "c Ʈ ´ ."]
-		Challenge 0, 1 is won by 1 by 4 points to 0.
-		Challenge 0, 2 is won by 2 for 3 points to 1.
-		Challenge 1, 2 is won by 1 by 3 points to 1.
-		In the end 0 has 0 challenges, 1 has 1 challenge and 2 has 2
-		challenges, so the final ranking will be [1, 2, 0].
-
-Each test is run with a 6s timeout (*2 on the VM)
-
-"""
-
-
 def ex(matches: list, k: int) -> list:
 	"""
 
 	A function that performs the "Who Screams Louder" game
 
 	:param matches: The list of sequences
-	:param k: Parameter K
+	:param k: Parameter K from the game
 	:return: A list containing the rankings
 	"""
 
-	n_matches = len(matches)
-	players = []
-	seqs = []
+	n_players = len(matches)  # Number of players
+	players = []  # Players stats (index, score and sequence sum)
+	seqs = []  # Sequences without whitespaces
+	whitespace = {  # Ignored characters
+		ord(" "): None,
+		ord("\t"): None
+	}
 
+	# Initialize sequences and stats
 	i = 0
-	while i < n_matches:
-		seqs.append(no_whitespace(matches[i]))
+	while i < n_players:
+		seqs.append(matches[i].translate(whitespace))
 		players.append({
 			"index": i,
 			"score": 0,
@@ -106,19 +29,19 @@ def ex(matches: list, k: int) -> list:
 		})
 		i += 1
 
+	# Play every match possible
 	i = 0
-	while i < n_matches - 1:
+	while i < n_players - 1:
 		j = i + 1
 
-		while j < n_matches:
+		while j < n_players:
 			match(seqs[i], seqs[j], players[i], players[j], k)
 
 			j += 1
-
 		i += 1
 
-	sorted_players = sorted(players, key=lambda player: player["score"], reverse=True)
-	return [player["index"] for player in sorted_players]
+	sorted_players = sorted(players, key=lambda player: player["score"], reverse=True)  # Sort by score
+	return [player["index"] for player in sorted_players]  # Return the original indexes only
 
 
 def match(seq1: str, seq2: str, player1: dict, player2: dict, k: int) -> None:
@@ -126,66 +49,67 @@ def match(seq1: str, seq2: str, player1: dict, player2: dict, k: int) -> None:
 
 	Compute a match of "Who Screams Louder"
 
-	:param seq1: First sequence
-	:param seq2: Second sequence
-	:param k: Parameter K
-	:return: 0 if seq1 wins, 1 otherwise
+	:param seq1: Player1's sequence
+	:param seq2: Player2's sequence
+	:param player1: Stats about player1
+	:param player2: Stats about player2
+	:param k: Parameter K from the game
+	:return: None. The winner will have the (stats') score increased
 	"""
 
-	# TODO Optimization: can just use positive/negative counter (scores seq1 add, scores seq2 sub)
-	point_seq1 = 0
-	point_seq2 = 0
+	points_delta = 0
 
 	i = 0
 	length = len(seq1)
 	while i < length:
 
-		oseq1 = ord(seq1[i])
-		oseq2 = ord(seq2[i])
-		delta = abs(oseq1 - oseq2)
+		val_char1 = ord(seq1[i])
+		val_char2 = ord(seq2[i])
+		val_delta = abs(val_char1 - val_char2)
 
-		if delta <= k:
-			if oseq1 > oseq2:
-				point_seq1 += 1
-			elif oseq2 > oseq1:
-				point_seq2 += 1
+		if val_delta <= k:
+			if val_char1 > val_char2:  # Character 1 wins
+				points_delta += 1
+			elif val_char2 > val_char1:  # Character 2 wins
+				points_delta -= 1
 		else:
-			if oseq1 < oseq2:
-				point_seq1 += 1
-			elif oseq2 < oseq1:
-				point_seq2 += 1
+			if val_char1 < val_char2:  # Character 1 wins
+				points_delta += 1
+			elif val_char2 < val_char1:  # Character 2 wins
+				points_delta -= 1
 
 		i += 1
 
-	if point_seq1 > point_seq2:
+	# First check: wins whoever has more points
+	if points_delta > 0:
 		player1["score"] += 1
-		return
-	elif point_seq2 > point_seq1:
+	elif points_delta < 0:
 		player2["score"] += 1
-		return
 
-	# TODO Optimization: store sum_seq in a record
-	if player1["seq_sum"] is None:
-		player1["seq_sum"] = seq_sum(seq1)
-	if player2["seq_sum"] is None:
-		player2["seq_sum"] = seq_sum(seq2)
-
-	sum_seq1 = player1["seq_sum"]
-	sum_seq2 = player2["seq_sum"]
-
-	if sum_seq1 < sum_seq2:
-		player1["score"] += 1
-		return
-	elif sum_seq2 < sum_seq1:
-		player2["score"] += 1
-		return
-
-	if seq1 < seq2:
-		player1["score"] += 1
-		return
+	# Draw: wins whoever has the least sequence total value
 	else:
-		player2["score"] += 1
-		return
+
+		# Calculate sequence value, if not already done, and store
+		if player1["seq_sum"] is None:
+			player1["seq_sum"] = seq_sum(seq1)
+		if player2["seq_sum"] is None:
+			player2["seq_sum"] = seq_sum(seq2)
+
+		sum_seq1 = player1["seq_sum"]
+		sum_seq2 = player2["seq_sum"]
+
+		if sum_seq1 < sum_seq2:
+			player1["score"] += 1
+		elif sum_seq2 < sum_seq1:
+			player2["score"] += 1
+
+		# Further draw: win by alphabetic order
+		elif seq1 < seq2:
+			player1["score"] += 1
+		else:
+			player2["score"] += 1
+
+	return
 
 
 def seq_sum(seq: str) -> int:
@@ -202,20 +126,3 @@ def seq_sum(seq: str) -> int:
 		acc += ord(char)
 
 	return acc
-
-
-def no_whitespace(seq: str) -> str:
-	"""
-
-	Removes all whitespace from a string
-
-	:param seq: The starting string
-	:return: The no-whitespace string
-	"""
-
-	whitespace = {
-		ord(" "): None,
-		ord("\t"): None
-	}
-
-	return seq.translate(whitespace)
