bee775fe6af901b46d66512cef6ce769f64a7725
excale <dev_excale@hotmail.com>
2021-11-23 17:34:27 +0100

    Optimizations on HW4

--- HW4-req/program01.py
+++ HW4-req/program01.py
@@ -19,30 +19,25 @@ def PoemSync(input_file: str, output_file: str, tau: int) -> float:
 	# Open the file and read all the lines, removing blank ones
 	# Also, remove every character which isn't a letter
 	with open(input_file, "r", encoding="utf-8") as input_stream:
-		lines = tuple(filter(None, map(edit_illegal_chars, input_stream)))
+		lines = tuple(filter(lambda string: len(string) > 1, map(edit_illegal_chars, input_stream)))
 
 	# Transmute the list of lines into a matrix of stresses,
 	# where each row is a line from the poem
 	matrix = tuple(flat(map(stresses_for, line.split())) for line in lines)
 
-	# Fill the blank spots in the matrix with 0's
-	max_len = max(map(len, matrix))
-	matrix = tuple(map(lambda row: tuple(row + ([0] * (max_len - len(row)))), matrix))
+	# Compute the sync value for every 2-rows combination
+	syncs = tuple(compute_sync(row1, row2, tau) for i, row1 in enumerate(matrix[:-1]) for row2 in matrix[i + 1:])
 
 	# Print the matrix to the specified file
 	output_matrix(output_file, matrix)
 
-	# Compute the sync value for every 2-rows combination (except with the same row)
-	syncs = []
-	for row1 in matrix:
-		for row2 in matrix:
-			if row1 is row2:
-				continue
-			root = sum(row1) * sum(row2)
-			syncs.append((c(row1, row2, tau) + c(row2, row1, tau)) / sqrt(root) / 2 if root else root)
-
 	# Return the even of syncs, rounded to the 6th decimal digit
-	return round(sum(syncs) / len(syncs), 6)
+	return round(sum(syncs) / len(syncs) / 2, 6)
+
+
+def compute_sync(row1: list, row2: list, tau: int) -> float:
+	root = sum(row1) * sum(row2)
+	return (c(row1, row2, tau) + c(row2, row1, tau)) / sqrt(root) if root else root
 
 
 def output_matrix(file_path: str, matrix: tuple) -> None:
@@ -54,9 +49,12 @@ def output_matrix(file_path: str, matrix: tuple) -> None:
 	:param matrix: The matrix(Iterable[Iterable[Any]])
 	:return: None
 	"""
+
+	# Print row and fill the blank spots in the matrix with 0's
+	max_len = max(map(len, matrix))
 	with open(file_path, "w", newline="\n", encoding="utf-8") as output_stream:
 		for line in matrix:
-			print("".join(map(str, line)), file=output_stream, end="\n")
+			print("".join(map(str, line)) + "0" * (max_len - len(line)), file=output_stream, end="\n")
 	return
 
 
@@ -70,7 +68,7 @@ def edit_illegal_chars(string: str) -> str:
 	:param string: The input string
 	:return: The edited string
 	"""
-	return "".join(char if char.isalpha() else " " if char in " '-" else "" for char in string)
+	return "".join(char if char.isalpha() else " " for char in string)
 
 
 def stresses_for(word: str) -> list:
@@ -84,7 +82,7 @@ def stresses_for(word: str) -> list:
 	:return: A list with the stresses
 	"""
 	phonetic = phones_for_word(word)
-	return map(int, stresses(phonetic[0]).translate({ 50: "0" }) + "0") if phonetic else [0] * (len(word) // 2 + 1)
+	return map(int, stresses(phonetic[0]).translate({50: "0"}) + "0") if phonetic else [0] * (len(word) // 2 + 1)
 
 
 def flat(arr: map) -> list:
@@ -105,7 +103,7 @@ def flat(arr: map) -> list:
 	return flattened
 
 
-def c(row1: tuple, row2: tuple, tau: int) -> int:
+def c(row1: list, row2: list, tau: int) -> int:
 	"""
 
 	Function c, used to compute the sync between two lines.
